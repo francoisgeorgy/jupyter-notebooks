1456807f8a323e117ff8b2ec0da35c3c793803ae
François Georgy <francois.georgy@gmail.com>
2022-09-15 12:25:29 +0200

    ac coupling

--- electronics/ac-coupling/utils/show.py
+++ electronics/ac-coupling/utils/show.py
@@ -2,7 +2,16 @@ import matplotlib.pyplot as pp
 import numpy as np
 import scipy.fftpack
 
-def plot(t, data, axes, title='', ymin=None, ymax=None):
+def cutoff(R, C):
+    return 1 / (2 * np.pi * C * R)
+
+def filter_C(R, f):
+    return 1 / (2 * np.pi * f * R)
+
+def filter_R(C, f):
+    return 1 / (2 * np.pi * f * C)
+
+def plot(t, data, axes, ymin=None, ymax=None, title=''):
     axes.plot(t, data)    
     if ymin is not None:
         axes.set_ylim(bottom=ymin)
@@ -11,7 +20,7 @@ def plot(t, data, axes, title='', ymin=None, ymax=None):
     axes.set_title(title)    
     axes.grid(True)
     
-def plot_fft(data, f, periods, axes, title=''):
+def plot_fft(data, f, periods, axes, ymin=None, ymax=None, title=''):
 
     samples = len(data)
     spacing = 1 / f * periods / samples
@@ -19,14 +28,26 @@ def plot_fft(data, f, periods, axes, title=''):
     
     y = scipy.fft.fft(data)
     samples = len(y)
-    axes.semilogy(x, 2.0/samples * np.abs(y[0:samples//2]))
+    
+    # fft_data = 2.0/samples * np.abs(y[0:samples//2])
+    # axes.semilogy(x, np.where(fft_data < 0.0001, 0.0, fft_data))
+    ## axes.semilogy(x, 2.0/samples * np.abs(y[0:samples//2]))
+
+    # fft_data = np.maximum(1.0e-6, 2.0/samples * np.abs(y[0:samples//2]))
+    # print(fft_data)
+    axes.semilogy(x, np.maximum(1.0e-6, 2.0/samples * np.abs(y[0:samples//2])))
+    
     # axes.plot(x, 2.0/samples * np.abs(y[0:samples//2]))
     axes.set_xlim(left=0, right=1000)
-    axes.set_ylim(10e-5, 10)
+    # axes.set_ylim(10e-5, 10)
+    if ymin is not None:
+        axes.set_ylim(bottom=ymin)
+    if ymax is not None:
+        axes.set_ylim(top=ymax)
     axes.set_title(title)
     axes.grid(True)
 
-def plot_fft_diff(data1, data2, f, periods, axes, title=''):
+def plot_fft_diff(data1, data2, f, periods, axes, ymin=None, ymax=None, title=''):
 
     if len(data1) != len(data2):
         print("ERROR: data not similar for plotting differences")
@@ -45,8 +66,13 @@ def plot_fft_diff(data1, data2, f, periods, axes, title=''):
     # print(2.0/samples * (np.abs(y1[0:samples//2]) - np.abs(y2[0:samples//2])))
     # for n in range(0, 100):
         # print(np.abs(y1[n]), np.abs(y2[n]), np.abs(y1[n]) - np.abs(y2[n]))
-    axes.plot(x, 2.0/samples * (np.abs(y1[0:samples//2]) - np.abs(y2[0:samples//2])))
+    # axes.plot(x, 2.0/samples * (np.abs(y1[0:samples//2]) - np.abs(y2[0:samples//2])))
+    axes.plot(x, np.maximum(1.0e-6, 2.0/samples * (np.abs(y1[0:samples//2]) - np.abs(y2[0:samples//2]))))
     axes.set_xlim(left=0, right=1000)
-    axes.set_ylim(10e-5, 10)
+    # axes.set_ylim(10e-5, 10)
+    if ymin is not None:
+        axes.set_ylim(bottom=ymin)
+    if ymax is not None:
+        axes.set_ylim(top=ymax)
     axes.set_title(title)
     axes.grid(True)
--- electronics/ac-coupling/utils/waves.py
+++ electronics/ac-coupling/utils/waves.py
@@ -1,9 +1,43 @@
 import numpy as np
 
-def get_squarewave(a, f, t):    
+def squarewave(a, f, t):
+    """
+    Return a periodic square-wave waveform.
+
+    Parameters
+    ----------
+    a : float
+        Amplitude
+    f : float
+        Frequency in [Hz]
+    t : array_like
+        The input time array.
+
+    Returns
+    -------
+        Output array containing the square waveform.
+    """
     return a * np.sign(np.sin(2 * np.pi * f * t))
 
 # periodic RC discharge
-def get_periodic_RC_discharge(R, C, t, f):
+def periodic_RC_discharge(R, C, t, f):
+    """
+    Return the RC discharge waveform.
+
+    Parameters
+    ----------
+    R : float
+        Resistance in [Ohm]
+    C : float
+        Capacitor in [F]
+    t : array_like
+        The input time array.
+    f : float
+        The frequency at which the discharge repeats.
+
+    Returns
+    -------
+        Output array containing the waveform.
+    """
     tau = R*C
     return np.exp(-(np.mod(t-0.00000001, (1/f)/2)) / tau)
